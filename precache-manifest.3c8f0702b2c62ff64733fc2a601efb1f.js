8cd073f3634d7ca0172d6ba5e777f5fa8efd3113
Muhammad Ramadhan <muhrama082@gmail.com>
2019-10-09 19:09:11 +0700

    fix bug

---
+++ precache-manifest.3c8f0702b2c62ff64733fc2a601efb1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100c9f9b020f00ed8b646f49590bed7e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f458403bb1d970ec0df",
+    "url": "/static/css/main.522d5c44.chunk.css"
+  },
+  {
+    "revision": "2ece7df84f62d489e65b",
+    "url": "/static/js/2.cc0dadad.chunk.js"
+  },
+  {
+    "revision": "0f458403bb1d970ec0df",
+    "url": "/static/js/main.a218d3b0.chunk.js"
+  },
+  {
+    "revision": "cdba8f2f3e51b99b9646",
+    "url": "/static/js/runtime-main.aeb2e43d.js"
+  },
+  {
+    "revision": "2eceeef0d8cbbd5cca2eaf99d5bc5b41",
+    "url": "/static/media/aneka-makanan.2eceeef0.jpg"
+  }
+]);
